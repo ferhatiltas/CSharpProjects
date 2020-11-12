87b77ad7aca76358fa54aff6dc3078a897d12809
Ferhat İltaş <iltasferhatt@gmail.com>
2020-11-12 12:11:37 +0300

    Add files via upload

---
+++ Converting Entered Degrees to Radians and Gradients/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degree,radyan,gradyan;
+            double pi = 3.14;
+            Console.Write("Dönüştürmek istediğiniz dereceyi giriniz.");
+            degree = Convert.ToDouble(Console.ReadLine());
+
+            radyan = degree * pi / 180;
+            gradyan = 400 * degree / 365;
+            Console.WriteLine("Girilen derecenin Gradyanı : " + gradyan + ", Radyanı : " + radyan);
+            Console.ReadKey();
+        }
+    }
+}
