628387f9829162c1605ce1cc58bb5105990c075a
Ferhat İltaş <iltasferhatt@gmail.com>
2020-11-14 11:26:52 +0300

    Add files via upload

---
+++ The Multiplication Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            for (x = 1; x <= 10; x++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(x + "x" + i + "=" + (x * i).ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
