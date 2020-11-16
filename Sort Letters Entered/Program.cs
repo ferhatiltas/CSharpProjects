985a1d626113f0755e3d2a42f6d7521e8de1abbe
Ferhat İltaş <iltasferhatt@gmail.com>
2020-11-16 23:01:22 +0300

    Add files via upload

---
+++ Sort Letters Entered/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            int i;
+            Console.Write("Enter a word : ");
+            word = Console.ReadLine();
+            int lenght = word.Length;
+            char[] dizi = new char[lenght];
+            for (i = 0; i < lenght; i++)
+            {
+                dizi[i] = word[i];
+            }
+            Array.Sort(dizi);
+            Console.WriteLine("****************************************");
+            Console.Write("In alphabetical order :");
+            for (i = 0; i < lenght; i++)
+            {
+                Console.Write(dizi[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
