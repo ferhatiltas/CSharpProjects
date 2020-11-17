9b5526e408d3d0ac45824c220b51b272e2084b87
Ferhat İltaş <iltasferhatt@gmail.com>
2020-11-17 18:31:55 +0300

    Add files via upload

---
+++ Making the Pascal triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of lines : ");
+            int line = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < line; i++)
+            {
+                int c = 1;
+                for (int e = 0; e < line - i; e++)
+                {
+                    Console.Write("   ");
+                }
+                for (int x = 0; x <= i; x++)
+                {
+                    Console.Write("    {0} ", c);
+                    c = c * (i - x) / (x + 1);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
